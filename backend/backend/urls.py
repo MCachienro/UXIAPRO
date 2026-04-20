833afc9df4aa2347b6cb4bd7d12a192b46d95b47
Dpcrack22 <davidperera2006@gmail.com>
2026-04-20 16:33:20 +0200

    models terminats

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from api.api import api as uxia_api
@@ -22,3 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', uxia_api.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
