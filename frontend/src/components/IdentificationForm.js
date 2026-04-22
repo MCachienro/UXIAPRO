a1f4f1a6938f5255f3cde6add7f5ccb3cac293b7
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-04-22 16:45:00 +0200

    Update IdentificationForm.js

--- frontend/src/components/IdentificationForm.js
+++ frontend/src/components/IdentificationForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || '/api';
 
 export default function IdentificationForm({ selectedExpoId }) {
   const [idFile, setIdFile] = useState(null);
